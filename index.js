dca550065cf27a58aef43e781afc770c900d7d65
Scott Bender <scott@scottbender.net>
2017-08-12 10:47:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,143 @@
+const Bacon = require("baconjs");
+const debug = require("debug")("signalk:signalk-kafka-gw");
+const util = require("util");
+const kafka = require('kafka-node')
+
+
+module.exports = function(app) {
+  var plugin = {};
+  var unsubscribes = [];
+
+  plugin.id = "signalk-kafka-gw";
+  plugin.name = "Kafka Gateway";
+  plugin.description = "Kafka Gateway";
+
+  plugin.onStop = [];
+
+  plugin.schema = {
+    title: 'Signal K - Kafka Gateway',
+    type: 'object',
+    required: ['port'],
+    properties: {
+      remoteHost: {
+        type: 'string',
+        title: 'Kafka Broker Url',
+        default: "localhost:2181"
+      },
+      interval: {
+        type: 'number',
+        title:
+        'Minimum interval between updates for this path to be sent to the server',
+        default: 1
+      },
+    },
+  };
+  
+  plugin.start = function(options) {
+    debug("start");
+    
+    plugin.onStop = [];
+    
+    var HighLevelProducer = kafka.HighLevelProducer;
+    plugin.client = new kafka.Client("localhost:2181"),
+    plugin.producer = new HighLevelProducer(plugin.client);
+
+    plugin.producer.on('ready', function () {
+      debug("ready")
+      startSending(options, plugin.producer, plugin.onStop)
+    });
+
+    plugin.producer.on('error', function(err) {
+      debug("error: " + err)
+    });
+  }
+
+  plugin.stop = function() {
+    plugin.onStop.forEach(f => f());
+    if ( plugin.client )
+      plugin.client.close()
+  };
+
+  return plugin;
+  
+  function startSending(options, producer, onStop) {
+    var command = {
+      context: "vessels.self",
+      subscribe: [{
+        path: "*",
+        period: options.interval * 1000
+      }]
+    }
+
+    debug("subscription: " + JSON.stringify(command))
+    
+    app.subscriptionmanager.subscribe(command,
+                                      onStop,
+                                      subscription_error,
+                                      got_delta);
+
+    /*
+    options.paths.forEach(pathInterval => {
+      onStop.push(
+        app.streambundle
+          .getSelfStream(pathInterval.path)
+          .debounceImmediate(pathInterval.interval * 1000)
+          .onValue(value => {
+            var payloads = [
+              {
+                topic: pathInterval.path,
+                messages:
+                JSON.stringify({
+                  context: 'vessels.' + app.selfId,
+                  timestamp: new Date(),
+                  path: pathInterval.path,
+                  value: value,
+                })
+              }]
+
+            debug("payloads: " + payloads[0].messages)
+            
+            producer.send(payloads, function (err, data) {
+              if ( err ) {
+                debug("send error: " + err)
+              }
+              debug("send data: " + JSON.stringify(data))
+            });
+          }))
+    });
+    */
+  }
+
+  function got_delta(delta) {
+    var payloads = [];
+
+    delta.updates.forEach(function(update) {
+      update.values.forEach(function(value) {
+        payloads.push({
+          topic: 'vessels.self.' + value.path,
+          messages:
+          JSON.stringify({
+            context: 'vessels.' + app.selfId,
+            timestamp: update.timestamp,
+            path: value.path,
+            value: value.value,
+          })
+        })
+      })
+    })
+    
+    //debug("payloads: " + JSON.stringify(payloads))
+    
+    plugin.producer.send(payloads, function (err, data) {
+      if ( err ) {
+        debug("send error: " + err)
+      }
+      //debug("send data: " + JSON.stringify(data))
+    })
+  }
+
+  function subscription_error(err)
+  {
+    debug("error: " + err)
+  }
+};
